64c30e2d60745289b187e8f33b0428678bef883d
Stefan Hagen <stefan@hagen.link>
2020-07-22 09:48:39 +0200

    Provided missing test argument

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,7 @@ import pytest  # type: ignore
 import nano_id.cli as cli
 
 
-def test_main_ok_size_and_alphabet_default():
+def test_main_ok_size_and_alphabet_default(capsys):
     assert cli.main([0, '']) is None
     out, err = capsys.readouterr()
     assert out == '0123456789abcdef'
