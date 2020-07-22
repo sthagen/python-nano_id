5bfd6c865d575519ffa9d33ee79dec5d32db084c
Stefan Hagen <stefan@hagen.link>
2020-07-22 09:55:33 +0200

    Added size conversion

--- nano_id/cli.py
+++ nano_id/cli.py
@@ -10,7 +10,7 @@ from nano_id.nano_id import create
 # pylint: disable=expression-not-assigned
 def main(argv=None):
     """Test driver for yet another implementation of nano id reading fromstdin and writing to stdout."""
-    size = sys.argv[1] if argv is None else argv[0]
+    size = int(sys.argv[1]) if argv is None else int(argv[0])
     alphabet = sys.argv[2] if argv is None else argv[1]
     sys.stdout.write(create(size, alphabet))
     
